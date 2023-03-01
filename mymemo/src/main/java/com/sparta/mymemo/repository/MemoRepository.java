ff45d713efdef3be223bd6100fb9f739569985f4
rhdqors <108318494+rhdqors@users.noreply.github.com>
2023-03-02 03:09:01 +0900

    Update MemoRepository.java

--- mymemo/src/main/java/com/sparta/mymemo/repository/MemoRepository.java
+++ mymemo/src/main/java/com/sparta/mymemo/repository/MemoRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     List<Memo> findAllByOrderByModifiedAtDesc();
+//    List<Memo> findByContentsAndTitleAndUserNameaAndCreatedAt
+//    여러개의 컬럼명을 가져오려면 위처럼 해야하는데 너무 비효율적
 }
